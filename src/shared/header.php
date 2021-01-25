dfcb67e4bb4d8f6399ddd679e879015566cb8265
jaugustomonteiro <jamonteirolima@gmail.com>
2021-01-25 18:34:02 +0000

    tipos de variaveis

--- src/shared/header.php
+++ src/shared/header.php
@@ -2,17 +2,20 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <?php require __DIR__ . "/inc/functions.php"; ?>    
+    <?php 
+        require __DIR__ . "/inc/functions.php"; 
+        $site_title = "PHP - Fullstack";
+    ?>    
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="http://<?=$server_host?>/src/shared/css/main.css">
-    <title>Document</title>
+    <title><?=$site_title?></title>
 </head>
 <body>
     
     <header class="lesson-header">
         <img src="http://<?=$server_host?>/src/shared/img/php-vscode-logo.png" alt="logo" style="display: block; height: 5rem; margin-bottom: 1.5rem">
-        PHP - UPSIDER
+        <?=$site_title?>
     </header>
     <div class="container">
    
--- src/shared/inc/functions.php
+++ src/shared/inc/functions.php
@@ -6,14 +6,21 @@ header("Content-Type: text/html; charset=utf-8");
 
 date_default_timezone_set("America/Sao_Paulo");
 
+set_error_handler("phpError");
+
 $server_host = $_SERVER['HTTP_HOST'];
+
+function phpError($error, $message, $file, $line) {
+    $color = ($error == E_USER_ERROR ? "red" : "yellow");
+    echo "<div class='message message-warning'>[ Linha {$line} ] <strong>{$message}</strong><small>{$file}</small></div>";
+}
 
 function heder_lesson($lesson = "lesson") {
     echo "<strong class='header-lesson'>{$lesson}</strong><hr />";
 }
 
 function lesson_title($title = "lesson", $line = __LINE__) {
-    echo "<h2 class='lesson-title'>[ {$title} | <span>Linha " . $line . "</span> ]</h2>";
+    echo "<h5 class='lesson-title'>[ {$title} | <span>Linha " . $line . "</span> ]</h5>";
 }
 
 function lesson_obs($title = "tag") {
