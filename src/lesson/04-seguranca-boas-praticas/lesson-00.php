11f6c4854bdb33c9db99a219183ca26d3527b162
jaugustomonteiro <jamonteirolima@gmail.com>
2021-01-30 20:09:57 +0000

    iniciando modulo 05

---
+++ src/lesson/04-seguranca-boas-praticas/lesson-00.php
@@ -0,0 +1,17 @@
+<?php
+
+require __DIR__ . "/../_shared/header.php";
+
+require __DIR__ . "/source/autoload.php";
+
+heder_lesson("Orientado a objeto <br> Subtítulo");
+
+lesson_title("Lesson", __LINE__);
+
+/*
+echo "<p>" . COURSE . "</p>";
+lesson_title($title = "include include_ondce", __LINE__);
+heder_lesson("Requisição de arquivos");
+lesson_obs("bolean");
+*/
+require __DIR__ . "/../_shared/footer.php";
